a9f48c017b4ba11fda5f3899a063eb7b1e63a2f8
Chattapon Uthum <bluenightz@Chattapons-MacBook-Pro.local>
2016-04-21 12:24:20 +0700

    ช่องค้นหา, รายการโปรด, แนะนำ, ปรับรายละเอียด
    เพิ่มเติมรายละเอียด
    - ช่องค้นหา (index.html, styles.css, javascripts/page_script.js)
    เพิ่ม slide ในส่วนของระยะทาง
    
    - รายการโปรด (index.html, styles.css)
    เพิ่ม badge เวลาบันทึกลงเป็นรายการโปรด
    
    - แนะนำ (index.html, styles.css)
    เพิ่ม badge เวลาเป็นร้านที่แนะนำ
    
    - จัดรูปแบบเวลา badge ทั้งสองปรากฏพร้อมกัน (styles.css)
    
    - ปรับรายละเอียดของ card ร้าน (กล่องที่แสดงแต่ละร้าน)  (index.html,
    styles.css)
    โดยเพิ่มบรรยายรายละเอียดของร้านลงไป ให้เหมือนกับตัวอย่าง mobile
    
    layout/default.html ไฟล์นี้ไม่ต้องสนใจ ไม่ได้ใช้

--- javascripts/page_script.js
+++ javascripts/page_script.js
@@ -6,12 +6,27 @@
 	      max: 500,
 	      values: [ 0, 500 ],
 	      slide: function( event, ui ) {
-	      	$("#min_num").html(ui.values[0]);
-	      	$("#max_num").html(ui.values[1]);
+	      	$("#price_min_num").html(ui.values[0]);
+	      	$("#price_max_num").html(ui.values[1]);
 	        // $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
 	      }
 	    });
-	      	$("#min_num").html( $("#slider_price").slider("values", 0));
-	      	$("#max_num").html( $("#slider_price").slider("values", 1));
+      	$("#price_min_num").html( $("#slider_price").slider("values", 0));
+      	$("#price_max_num").html( $("#slider_price").slider("values", 1));
+
+
+		$("#slider_dist").slider({
+	      range: true,
+	      min: 0,
+	      max: 50,
+	      values: [ 0, 50 ],
+	      slide: function( event, ui ) {
+	      	$("#dist_min_num").html(ui.values[0]);
+	      	$("#dist_max_num").html(ui.values[1]);
+	        // $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	      }
+	    });
+      	$("#dist_min_num").html( $("#slider_dist").slider("values", 0));
+      	$("#dist_max_num").html( $("#slider_dist").slider("values", 1));
 	})
 })(jQuery);

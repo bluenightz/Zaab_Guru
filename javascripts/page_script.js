98153cc77f1b248dc0cee08ca74abc7557f6b6a4
Chattapon Uthum <bluenightz@Chattapons-MacBook-Pro.local>
2016-04-21 18:29:32 +0700

    เพิ่มหน้า ดูรายละเอียดร้าน
    - เพิ่มหน้า ดูรายละเอียดร้าน
    - อัพเดท javascripts/libs/jquery-ui เนื่องจากมีการเพิ่ม widget
    - เพิ่ม images/food
    - แก้ไข styleshees/styles.css, javascript/page_script.js

--- javascripts/page_script.js
+++ javascripts/page_script.js
@@ -28,5 +28,29 @@
 	    });
       	$("#dist_min_num").html( $("#slider_dist").slider("values", 0));
       	$("#dist_max_num").html( $("#slider_dist").slider("values", 1));
+
+
+      	$(".btn-enter-restaurant").click(function(e){
+      		open_popupcontainer();
+      	});
+
+      	$(".popupcontainer__close").click(function(e){
+      		close_popupcontainer();
+      	});
+
+      	function open_popupcontainer(){
+      		$(".container-fluid").addClass("popup__open");
+      		$(".popupcontainer").css("top", $(".contentbox").scrollTop() );
+      	}
+      	function close_popupcontainer(){
+      		$(".container-fluid").removeClass("popup__open");
+      	}
+
+      	$(".foodlist__number input").spinner({
+      		step: 1,
+      		min: 0,
+      		numberFormat: "n"
+      	});
+
 	})
 })(jQuery);

6512355e54c6489a32deb2e26ff8e72e075cdecc
Chattapon Uthum <bluenightz@Chattapons-MacBook-Pro.local>
2016-05-03 00:18:39 +0700

    เพิ่มการเปลี่ยนจำนวนอาหารโดยการพิมพ์
    เพิ่มการเปลี่ยนจำนวนอาหารโดยการพิมพ์
    ที่ไฟล์ js/popupcontainer.js

--- js/popupcontainer.js
+++ js/popupcontainer.js
@@ -9,7 +9,7 @@
 			"code": [], 
 			"item": {}
 		};
-
+		//
 		// {
 		// 	"shopid": 1,
 		// 	"code": [1, 4], 
@@ -44,23 +44,37 @@
 					      		step: 1,
 					      		min: 0,
 					      		numberFormat: "n",
+					      		change: function(e, ui){
+					      			applycart(e, ui, "change");
+					      		},
 					      		spin: function(e, ui){
-					      			var _target = $(e.target);
-					      			var _parent = _target.closest(".foodlist__detail");
-					      			if( _cart.code.indexOf( _target.data("idfood") ) == -1 ){
-					      				_cart.code.push( _target.data("idfood") );
-					      			}
-				      				_cart.item[ String(_target.data("idfood")) ] = {
-				      					price : _parent.find(".foodlist__price").text().trim().match(/[,\.\d]+/g)[0],
-				      					foodname : _parent.find(".foodlist__title").text(),
-				      					quantity : ui.value
-				      				};
+					      			applycart(e, ui);
 					      		}
 					      	});
 		  				}
 		  			});
 		  		}, 600);
   			break;
+  		} 
+
+
+  		function applycart(e, ui, type){
+  			var _value;
+  			if( type == 'change' ){
+  				_value = $(e.target).val();
+  			}else{
+  				_value = ui.value;
+  			}
+  			var _target = $(e.target);
+  			var _parent = _target.closest(".foodlist__detail");
+  			if( _cart.code.indexOf( _target.data("idfood") ) == -1 ){
+  				_cart.code.push( _target.data("idfood") );
+  			}
+				_cart.item[ String(_target.data("idfood")) ] = {
+					price : _parent.find(".foodlist__price").text().trim().match(/[,\.\d]+/g)[0],
+					foodname : _parent.find(".foodlist__title").text(),
+					quantity : _value
+				};
   		}
 
   		function getCart(){

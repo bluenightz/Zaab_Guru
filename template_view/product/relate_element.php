59b87a7e5cbda757304b2a38dd7a41ec5288c44a
Chattapon Uthum <bluenightz@hotmail.com>
2016-06-13 12:38:58 +0700

    Delete template files

--- template_view/product/relate_element.php
+++
@@ -1,5 +0,0 @@
-<select style="width:500px;" data-placeholder="Relate Product..." class="chosen-select" multiple name="<?php echo $element_attrbute_1['name']; ?>" tabindex="4">
-    <?php foreach ($element_attrbute_1['data'] as $key => $value) {  ?>
-    	<option <?php echo ( in_array( $value['id'] , $element_attrbute_1['selected']) ?' selected ':''); ?> value="<?php echo $value['id']; ?>"><?php echo $value['title']; ?></option>
-    <?php } ?>
-</select>
